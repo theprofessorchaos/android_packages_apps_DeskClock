121995169f1f49ef79f92e42d941e244569a54ec
James Lemieux <jplemieux@google.com>
2015-07-27 18:15:21 +0000

    Merge "Add option for progressive ringtones" into ub-deskclock-charm Conflicts: 	src/com/android/deskclock/SettingsActivity.java

--- src/com/android/deskclock/SettingsActivity.java
+++ src/com/android/deskclock/SettingsActivity.java
@@ -49,6 +49,8 @@ public class SettingsActivity extends BaseActivity {
 
     public static final String KEY_ALARM_SNOOZE = "snooze_duration";
     public static final String KEY_ALARM_VOLUME = "volume_setting";
+    public static final String KEY_ALARM_CRESCENDO = "alarm_crescendo_duration";
+    public static final String KEY_TIMER_CRESCENDO = "timer_crescendo_duration";
     public static final String KEY_TIMER_RINGTONE = "timer_ringtone";
     public static final String KEY_VOLUME_BEHAVIOR = "volume_button_setting";
     public static final String KEY_FLIP_ACTION = "flip_action";
@@ -298,6 +300,14 @@ public class SettingsActivity extends BaseActivity {
             final SnoozeLengthDialog snoozePref =
                     (SnoozeLengthDialog) findPreference(KEY_ALARM_SNOOZE);
             snoozePref.setSummary();
+
+            final CrescendoLengthDialog alarmCrescendoPref =
+                    (CrescendoLengthDialog) findPreference(KEY_ALARM_CRESCENDO);
+            alarmCrescendoPref.setSummary();
+
+            final CrescendoLengthDialog timerCrescendoPref =
+                    (CrescendoLengthDialog) findPreference(KEY_TIMER_CRESCENDO);
+            timerCrescendoPref.setSummary();
 
             final ListPreference weekStartPref = (ListPreference) findPreference(KEY_WEEK_START);
             // Set the default value programmatically
